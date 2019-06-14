e5bdcd2d3c4301bc864ece77d56a352def24bfc4
wangaoxin <wangaoxin@lvjitec.com>
2019-06-14 16:37:17 +0800

    fc

---
+++ i18n/index.js
@@ -0,0 +1,41 @@
+import {I18nManager } from 'react-native';
+import * as RNLocalize from "react-native-localize";
+import i18n from "i18n-js";
+import memoize from "lodash.memoize";
+
+const fallback = { languageTag: "en", isRTL: false }
+
+
+// 缓存翻译的结果
+export const translate = memoize(
+  (key, config) => i18n.t(key, config),
+  (key, config) => (config ? key + JSON.stringify(config) : key),
+);
+
+const translationGetters = {
+  // lazy requires (metro bundler does not support symlinks)
+  en: () => require("./en.json"),
+  // 'zh-CN': () => require("./zh-CN.json"),
+  'zh-TW': () => require("./zh-TW.json"),
+};
+
+
+export const setI18nConfig = () => {
+  // fallback if no available language fits
+  const fallback = { languageTag: "en", isRTL: false };
+
+  const { languageTag, isRTL } = RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) || fallback;
+
+  // clear translation cache
+  translate.cache.clear();
+  // update layout direction
+  I18nManager.forceRTL(isRTL);
+
+  console.log('languageTag',languageTag)
+
+  // set i18n-js config
+  i18n.translations = { [languageTag]: translationGetters[languageTag]() };
+  i18n.locale = languageTag;
+};
+
+export default i18n;
