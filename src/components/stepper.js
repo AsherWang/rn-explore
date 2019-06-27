b457c471ee60999ec3da7ebaeb9bb12a0b19351a
wangaoxin <wangaoxin@lvjitec.com>
2019-06-27 21:09:52 +0800

    refactor: hooks style

--- src/components/stepper.js
+++ src/components/stepper.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import React, { Component } from 'react';
+import React, { useCallback } from 'react';
 import { Button, Text, Icon } from 'native-base';
 import { View, StyleSheet } from 'react-native';
 import _throttle from 'lodash.throttle';
@@ -20,39 +20,39 @@ const styles = StyleSheet.create({
   },
 });
 
-class Stepper extends Component {
-  constructor(props) {
-    super(props);
-    const { onValueChange, throttle } = props;
-    this.onValueChangeThrottled = _throttle((nv) => {
+function Stepper(props) {
+  const {
+    onValueChange, throttle, value, style,
+  } = props;
+
+  const onValueChangeThrottled = useCallback(
+    _throttle((nv) => {
       if (onValueChange) {
         onValueChange(nv);
       }
-    }, throttle, { trailing: false });
-  }
+    }, throttle, { trailing: false }),
+    [throttle],
+  );
 
-  render() {
-    const { value, style } = this.props;
-    return (
-      <View style={{ ...styles.cont, ...style }}>
-        <Button
-          onPress={() => this.onValueChangeThrottled(value - 1)}
-          style={styles.btn}
-        >
-          <Icon name="remove" />
-        </Button>
-        <View style={styles.text}>
-          <Text>{value}</Text>
-        </View>
-        <Button
-          onPress={() => this.onValueChangeThrottled(value + 1)}
-          style={styles.btn}
-        >
-          <Icon name="add" />
-        </Button>
+  return (
+    <View style={{ ...styles.cont, ...style }}>
+      <Button
+        onPress={() => onValueChangeThrottled(value - 1)}
+        style={styles.btn}
+      >
+        <Icon name="remove" />
+      </Button>
+      <View style={styles.text}>
+        <Text>{value}</Text>
       </View>
-    );
-  }
+      <Button
+        onPress={() => onValueChangeThrottled(value + 1)}
+        style={styles.btn}
+      >
+        <Icon name="add" />
+      </Button>
+    </View>
+  );
 }
 
 Stepper.defaultProps = {
